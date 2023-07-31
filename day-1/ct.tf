9f62f78c5e470eaf016eba9499ecea9c5664cbe4
Martins Eglitis <sitilge@gmail.com>
2023-07-31 20:29:35 +0300

    Add TF resources for adding a new bucket and new service account bindings

---
+++ day-1/ct.tf
@@ -0,0 +1,33 @@
+#See main.tf for service account roles for ct
+resource "google_service_account" "ct" {
+  account_id   = "${var.name}-ct"
+  display_name = "${var.name}-ct"
+}
+
+resource "google_service_account_iam_binding" "staging" {
+  members = [
+    "serviceAccount:${var.google_project}.svc.id.goog[staging/default]"
+  ]
+  role               = "roles/storage.objectCreator"
+  service_account_id = google_service_account.ct.name
+}
+
+resource "google_service_account_iam_binding" "production" {
+  members = [
+    "serviceAccount:${var.google_project}.svc.id.goog[production/default]"
+  ]
+  role               = "roles/storage.objectCreator"
+  service_account_id = google_service_account.ct.name
+}
+
+resource "google_storage_bucket" "ct" {
+  location                    = var.google_region
+  force_destroy               = false
+  name                        = "${var.name}-ct"
+  public_access_prevention    = "inherited"
+  uniform_bucket_level_access = true
+
+  autoclass {
+    enabled = true
+  }
+}
