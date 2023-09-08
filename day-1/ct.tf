9d94d754af7bfaadc766e2d7ba07cea6c222366d
Martins Eglitis <sitilge@gmail.com>
2023-09-08 19:48:26 +0300

    Create separate service account for CT staging and production

--- day-1/ct.tf
+++ day-1/ct.tf
@@ -1,7 +1,12 @@
 #See main.tf for service account roles for ct
-resource "google_service_account" "ct" {
-  account_id   = "${var.name}-ct"
-  display_name = "${var.name}-ct"
+resource "google_service_account" "ct_staging" {
+  account_id   = "${var.name}-ct-staging"
+  display_name = "${var.name}-ct-staging"
+}
+
+resource "google_service_account" "ct_production" {
+  account_id   = "${var.name}-ct-production"
+  display_name = "${var.name}-ct-production"
 }
 
 resource "google_service_account_iam_binding" "staging" {
@@ -9,7 +14,7 @@ resource "google_service_account_iam_binding" "staging" {
     "serviceAccount:${var.google_project}.svc.id.goog[staging/staging-economic-handler]"
   ]
   role               = "roles/iam.workloadIdentityUser"
-  service_account_id = google_service_account.ct.name
+  service_account_id = google_service_account.ct_staging.name
 }
 
 resource "google_service_account_iam_binding" "production" {
@@ -17,7 +22,7 @@ resource "google_service_account_iam_binding" "production" {
     "serviceAccount:${var.google_project}.svc.id.goog[production/production-economic-handler]"
   ]
   role               = "roles/iam.workloadIdentityUser"
-  service_account_id = google_service_account.ct.name
+  service_account_id = google_service_account.ct_production.name
 }
 
 resource "google_storage_bucket" "ct" {
